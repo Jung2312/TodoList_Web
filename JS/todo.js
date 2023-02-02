f95ead656228309b886508fb5ac11036d1ef4a97
JUNG\alswj <20212856@office.deu.ac.kr>
2023-02-03 00:09:32 +0900

    디자인 변경, 메뉴 변경

--- JS/todo.js
+++ JS/todo.js
@@ -7,60 +7,62 @@ const todobtn = document.querySelector("#todobtn");
 
 let todo = [];
 
-function onclicktodo()
-{
-    const clickClass = "hidden";
-    todohome.classList.remove(clickClass);// 클래스가 존재하면 제거, 존재하지 않으면 추가
-    // 다른 메뉴 클릭시 클래스 추가
-}
+let check = [];
 
+function onclicktodo() {
+  const clickClass = "hidden";
+  todohome.classList.remove(clickClass); // 클래스가 존재하면 제거, 존재하지 않으면 추가
+  // 다른 메뉴 클릭시 클래스 추가
+}
 
 todobtn.addEventListener("click", onclicktodo);
 
 const TODOLIST = "TODOLIST";
 
-function saveToDo()
-{
-    localStorage.setItem(TODOLIST, JSON.stringify(todo));
+function saveToDo() {
+  localStorage.setItem(TODOLIST, JSON.stringify(todo));
 }
 
-function deleteToDo(event)
-{
-    const li = event.target.parentElement;
-    li.remove();
-    todo = todo.filter((todos) => todos.id !== parseInt(li.id)); // filter : 조건에 해당하는 값으로 새로운 배열 생성
-    saveToDo(); 
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
+  todo = todo.filter((todos) => todos.id !== parseInt(li.id)); // filter : 조건에 해당하는 값으로 새로운 배열 생성
+  saveToDo();
 }
 
-function paintToDo(todoValue)
-{
-    const li = document.createElement("li");
-    li.id = todoValue.id // li 생성 시 id를 줌
-    const span = document.createElement("span"); // 삭제 버튼을 넣어므로 span에 텍스트 추가
-    span.innerText = todoValue.text; // 오브젝트의 텍스트만 추출
+function paintToDo(todoValue) {
+  const li = document.createElement("li");
+  li.id = todoValue.id; // li 생성 시 id를 줌
 
-    const button = document.createElement("button");
-    button.addEventListener("click", deleteToDo);
-    button.innerText= "🗑️";
-    li.appendChild(span);
-    li.appendChild(button);
-    todoList.appendChild(li);
+  const input = document.createElement("input");
+  input.id = "todocheck";
+  input.type = "checkbox";
+
+  const span = document.createElement("span"); // 삭제 버튼을 넣어므로 span에 텍스트 추가
+  span.innerText = todoValue.text; // 오브젝트의 텍스트만 추출
+
+  const button = document.createElement("button");
+  button.addEventListener("click", deleteToDo);
+  button.innerText = "🗑️";
+  li.appendChild(input);
+  li.appendChild(span);
+  li.appendChild(button);
+  todoList.appendChild(li);
 }
 
-function handleToDoSubmit(event)
-{
-    event.preventDefault();
-    const todoValue = todoInput.value;
-    todoInput.value = "";
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const todoValue = todoInput.value;
+  todoInput.value = "";
 
-    const newToDoObj = {
-        text:todoValue,
-        id : Date.now()
-    };
+  const newToDoObj = {
+    text: todoValue,
+    id: Date.now(),
+  };
 
-    todo.push(newToDoObj);
-    paintToDo(newToDoObj);
-    saveToDo();
+  todo.push(newToDoObj);
+  paintToDo(newToDoObj);
+  saveToDo();
 }
 
 todoForm.addEventListener("submit", handleToDoSubmit);
@@ -68,9 +70,8 @@ todoplus.addEventListener("click", handleToDoSubmit);
 
 const savedToDo = localStorage.getItem(TODOLIST);
 
-if(savedToDo !== null)
-{
-    const parseToDo = JSON.parse(savedToDo);
-    todo = parseToDo; // 배열 복사
-    parseToDo.forEach(paintToDo); // js는 item을 자동으로 전달하므로 기존 메소드 사용
+if (savedToDo !== null) {
+  const parseToDo = JSON.parse(savedToDo);
+  todo = parseToDo; // 배열 복사
+  parseToDo.forEach(paintToDo); // js는 item을 자동으로 전달하므로 기존 메소드 사용
 }
